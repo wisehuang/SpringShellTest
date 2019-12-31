7c877818aff35ca8b276daecb968cb7a7edbbaad
Jonathan <jonathan.cc_huang@htc.com>
2019-12-31 10:31:47 +0800

    1. Update build.gradle for better configuration 2. Support Java 11

--- src/main/java/com/line/pay/springshelltest/SpringShellTest.java
+++ src/main/java/com/line/pay/springshelltest/SpringShellTest.java
@@ -1,8 +1,12 @@
 package com.line.pay.springshelltest;
 
+import org.jline.utils.AttributedString;
+import org.jline.utils.AttributedStyle;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.shell.jline.PromptProvider;
 
 @SpringBootApplication
 public class SpringShellTest {
@@ -10,8 +14,8 @@ public class SpringShellTest {
         ConfigurableApplicationContext context = SpringApplication.run(SpringShellTest.class, args);
     }
 
-//    @Bean
-//    public PromptProvider promptProvider() {
-//        return () -> AttributedString("shell:>", AttributedStyle.DEFAULT.foreground(AttributedStyle.YELLOW));
-//    }
+    @Bean
+    public PromptProvider promptProvider() {
+        return () -> new AttributedString("shell:>", AttributedStyle.DEFAULT.foreground(AttributedStyle.YELLOW));
+    }
 }
